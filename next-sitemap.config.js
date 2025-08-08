c51749eae1f2475c9cd4d976eabccc41cef2a8ca
Leandro Barbosa <leandrobarbosa@sfr.fr>
2025-08-08 16:22:23 +0200

    added good index

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.massiliaradio.live",
+  generateRobotsTxt: true,
+  changefreq: "weekly",
+  priority: 0.7,
+  sitemapSize: 7000,
+};
